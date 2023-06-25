bee1e9b9f118b3fc59b742fe4378c40960c70fbc
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-06-26 04:26:33 +0530

    Added JSX Support

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,11 @@
 export const Greet = () => {
-  return <h1>Hello Sushil</h1>;
+  return (
+    <h1>
+      <h1>Hello Sushil</h1>
+      <h2>Evaluating JSX</h2>
+      <h3>2+2 = {2 + 2}</h3>
+    </h1>
+  );
 };
 
 export default { Greet };
