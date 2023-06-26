8c3ee507512f68aec4656b2da00bfd0fbf8dc4bb
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-06-27 04:08:19 +0530

    Different types of css styling in REACT

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import { ClickHandler } from "./components/ClickHandler";
 import { ParentComponenet } from "./components/ParentComponenet";
 import { UserGreeting } from "./components/UserGreeting";
 import { NameList } from "./components/NameList";
+import { Stylesheet } from "./components/Stylesheet";
+import { InLine } from "./components/InLine";
+// import "./appStyles.css";
+// import styles from "./appStyles.module.css";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
 
 function App() {
   return (
@@ -21,7 +27,11 @@ function App() {
       {/* <ClickHandler></ClickHandler> */}
       {/* <ParentComponenet></ParentComponenet> */}
       {/* <UserGreeting></UserGreeting> */}
-      <NameList></NameList>
+      {/* <NameList></NameList> */}
+      <Stylesheet></Stylesheet>
+      <InLine></InLine>
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
     </div>
   );
 }
---
+++ src/components/InLine.js
@@ -0,0 +1,7 @@
+export const InLine = () => {
+  const headling = {
+    fontSize: "72px",
+    color: "blue",
+  };
+  return <h2 style={headling}>Inline</h2>;
+};
